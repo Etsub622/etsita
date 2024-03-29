c5b60a7c620fe1b03eadaac9bfbbbba7358cc740
Etsubie06 <etsubtaye4@gmail.com>
2024-03-28 22:51:21 -0800

    added new feature

---
+++ contactpage/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ContactPage from "./components/ContactPage";
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <ContactPage />
+      </main>
+    </div>
+  );
+}
+
+export default App;
